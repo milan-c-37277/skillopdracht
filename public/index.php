e9510d822ed8e98e0d546b993210010e49d46de6
milan-c-37277 <37277@ma-web.nl>
2025-09-01 13:38:49 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ $result = $conn->query($sql);
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Skillsopdracht Draggable Grid</title>
+  <title>Skillsopdracht</title>
   <link rel="stylesheet" href="/assets/css/style.css" />
 </head>
 <body>
